6d0a3e0f3e954666de256b39c97467852534f7bb
Aitor Ortega Morón <6357185002@edu.gencat.cat>
2024-10-23 15:51:28 +0200

    Anys a segons corregida

---
+++ anys a segons.py
@@ -0,0 +1,8 @@
+nom = "Aitor"
+anys = 16
+mesos = anys*12
+setmanes = mesos*4
+dies = setmanes*7
+hores = dies*24
+segons = hores*3600
+print('L\'' + str(nom) + ' té ' + str(anys) + ' anys' + ' i, ' + 'per tant, ' + 'porta viu ' + str(segons) + ' segons.')
